2c92cae656f1900dddf2988b93afa72649e22cab
asdtae <112775132+asdtae@users.noreply.github.com>
2025-10-21 23:54:55 +0300

    lab1/01 done + lab1/02 init

--- Lab1/01/main.cpp
+++ Lab1/01/main.cpp
@@ -1,4 +1,17 @@
+/*
+Mathe Ruben-Jonathan
+512/1
+Lab1/01
+
+Kijelentés:
+    Hasonlítsuk össze két egész típusú változó cseréjének sebességét három különböző módszerrel:
+    • segédváltozóval
+    • összeadással és kivonással
+    • xor cserével
+
+*/
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
@@ -13,20 +26,61 @@ void csereSegedValtozoval(unsigned long int &a, unsigned long int &b)
 
 void csereOsszeadasKivonas(unsigned long int &a, unsigned long int &b)
 {
-    a += b;
+    if(a > b)
+    {
+        a -= b;
+        b += a;
+        a = b - a;
+    }
+    else
+    {
+        b -= a;
+        a += b;
+        b = a -b;
+    }
+}
 
+void xorcsere(unsigned long int &a, unsigned long int &b)
+{
+    a = b ^ a;
+    b = a ^ b;
+    a = b ^ a;
 }
 
 int main()
 {
-    unsigned long int elsoSzam, masodikSzam;
+    unsigned long int szam1, szam2;
 
-    cin >> elsoSzam >> masodikSzam;
+    cin >> szam1 >> szam2;
 
-    csereSegedValtozoval(elsoSzam,masodikSzam);
+    // csere seged valtozoval
+    clock_t kezdoIdo1 = clock();
+    for(int i=0; i<10000000; i++)
+    {
+        csereSegedValtozoval(szam1, szam2);
+    }
+    clock_t vegsoIdo1 = clock();
 
-    swap()
+    // csere + -
+    clock_t kezdoIdo2 = clock();
+    for(int i=0; i<10000000; i++)
+    {
+        csereOsszeadasKivonas(szam1,szam2);
+    }
+    clock_t vegsoIdo2 = clock();
 
-    cout << elsoSzam << ' ' << masodikSzam;
+    // csere xor
+    clock_t kezdoIdo3 = clock();
+    for(int i=0; i<10000000; i++)
+    {
+        xorcsere(szam1,szam2);
+    }
+    clock_t vegsoIdo3 = clock();
+
+    xorcsere(szam1,szam2);
+
+    cout << szam1 << ' ' << szam2 << '\n';
+    cerr << float(vegsoIdo1 - kezdoIdo1) / (float)CLOCKS_PER_SEC << '\n' << float(vegsoIdo2 - kezdoIdo2) / (float)CLOCKS_PER_SEC << '\n' << float(vegsoIdo3 - kezdoIdo3) / (float)CLOCKS_PER_SEC << '\n';
+
     return 0;
 }
---
+++ Lab1/02/main.cpp
@@ -0,0 +1,22 @@
+/*
+Mathe Ruben-Jonathan
+512/1
+Lab1/02
+
+Kijelentés:
+    Hasonlítsuk össze egy ismétlődő ciklus sebességét amely a beolvasott szám gyökéig megy el, háromféleképpen implementálva:
+        • while (i*i <= n)
+        • sqrt(n)-t segédváltozóba mentve a ciklus előtt
+        • while (i <= sqrt(n))
+    Teszteljük a három változatot n különböző értékeire 10 és 1 000 000 000 között.
+
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned long int n;
+
+}
